9f63ff2a186009c3993091cb4718f346ab4681a8
Soban Malik <sm280821@gmail.com>
2022-09-17 16:37:57 +0500

    added ugly ui

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const choices = ['SCISSORS', 'ROCK', 'PAPER']
-let compChoice;
+const playerScore = document.querySelector('.playerScore');
+const compScore = document.querySelector('.compScore');
 let pwins = 0; let cwins = 0;
+let compChoice;
 let playerChoice;
 let rockBtn = document.querySelector('.rock');
 let paperBtn = document.querySelector('.paper');
@@ -9,46 +11,66 @@ let scissorsBtn = document.querySelector('.scissors');
 function getComputerChoice () {
     compChoice = Math.floor(Math.random() * choices.length);
     compChoice = choices[compChoice];
-    console.log(compChoice);
+    document.querySelector('.compChoice').innerHTML = compChoice;
 
+}
+
+function winner() {
+    if (pwins == 5) {
+        document.querySelector('.winner').innerHTML = 'Player is winner';
+        document.querySelector('.compChoice').innerHTML = '';
+        console.clear();
+        pwins = 0;
+        cwins = 0;
+        playerScore.innerHTML = pwins;
+        compScore.innerHTML = cwins;
+    } else if (cwins == 5) {
+        document.querySelector('.winner').innerHTML = 'Computer is winner';
+        document.querySelector('.compChoice').innerHTML = '';
+        console.clear();
+        pwins = 0;
+        cwins = 0;
+        playerScore.innerHTML = pwins;
+        compScore.innerHTML = cwins;
+    }
 }
 
 function playRound (pChoice, cChoice) {
     if (pChoice == cChoice){
             console.log('Tie');
-            return pwins += 1, cwins += 1;
+            playerScore.innerHTML = pwins;
+            compScore.innerHTML = cwins;
     } else if (pChoice == 'ROCK') {
         if (cChoice == 'PAPER') {
             console.log('You Lost! Paper beats Rock');
-            return cwins += 1;
+            cwins += 1;
+            compScore.innerHTML = cwins;
         } else if (cChoice == 'SCISSORS'){
             console.log('You Won! Rock beats Scissors');
-            return pwins += 1;
+            pwins += 1;
+            playerScore.innerHTML = pwins;
         }
     } else if (pChoice == 'PAPER') {
         if (cChoice == 'SCISSORS') {
             console.log('You Lost! Scissors bets Paper');
-            return cwins += 1;
+            cwins += 1;
+            compScore.innerHTML = cwins;
         } else if (cChoice == 'ROCK'){
             console.log('You Won! Paper bets Rocks');
-            return pwins += 1;
+            pwins += 1;
+            playerScore.innerHTML = pwins;
         }
     } else if (pChoice == 'SCISSORS') {
         if (cChoice == 'ROCK') {
             console.log('You Lost! Rock beats Scissors');
-            return cwins += 1;
+            cwins += 1;
+            compScore.innerHTML = cwins;
             } else if (cChoice == 'PAPER'){
             console.log('You Won! Scissors beats Paper');
-            return pwins += 1;
+            pwins += 1;
+            playerScore.innerHTML = pwins;
             }
         }
+    
+    document.querySelector('.winner').innerHTML = '';
 }
-
-rockBtn.addEventListener('click', getComputerChoice);
-rockBtn.addEventListener('click', function () {playRound('ROCK', compChoice)});
-
-paperBtn.addEventListener('click', getComputerChoice);
-paperBtn.addEventListener('click', function () {playRound('PAPER', compChoice)});
-
-scissorsBtn.addEventListener('click', getComputerChoice);
-scissorsBtn.addEventListener('click', function () {playRound('SCISSORS', compChoice)});
